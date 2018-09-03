bd13d00e2064fee24f42e870ff668b78f887e673
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-03 21:30:50 +0000

    We used request package to download an image and save it to file.

---
+++ down.js
@@ -0,0 +1,18 @@
+// require `request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+
+console.log('Downloading image...');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+       .on('error', function (err) {
+         throw err; 
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .on('end', function (response) {
+        console.log('Download complete.');
+      })
+       .pipe(fs.createWriteStream('./future.jpg')
+    );   
